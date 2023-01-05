b6cd35eb6193d05515b69108094264b9e87776c3
Maxence Bon <maxence.bon@orange.fr>
2023-01-05 17:03:08 +0100

    tp interface

--- src/test/java/java8/ex01/Method_01_Test.java
+++ src/test/java/java8/ex01/Method_01_Test.java
@@ -19,6 +19,15 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+    
+        public default int sumAge() {
+        	
+        	int somme = 0;
+        	for (int i=0; i<findAll().size(); i++) {
+        		somme += findAll().get(i).getAge();
+        	}
+        	return somme;
+        }
     }
     // end::IDao[]
 
@@ -48,8 +57,8 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
-
+        int result = daoA.sumAge();
+         System.out.println(result); 
         assert result == 210;
     }
 
@@ -59,7 +68,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
